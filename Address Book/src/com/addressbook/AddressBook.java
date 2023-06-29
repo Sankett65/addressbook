729da0bc54cbabb2c9acaa5c26db238aea6d8908
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-06-29 23:29:19 +0530

    Ability to Create contact in Adress book

--- Address Book/src/com/addressbook/AddressBook.java
+++ Address Book/src/com/addressbook/AddressBook.java
@@ -13,61 +13,46 @@ class Contact{
     private int zipNo;
     private int phoneNumber;
     private String email;
+    private static ArrayList<Contact> list = new ArrayList<Contact>();
+
+
+    public Contact(String firstName, String lastName, String address, int zipNo, int phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.zipNo = zipNo;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public int getZipNo() {
         return zipNo;
     }
 
-    public void setZipNo(int zipNo) {
-        this.zipNo = zipNo;
-    }
-
     public int getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-   
 
 }
 public class AddressBook {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book");
 
     }
 }
